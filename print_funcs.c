59d61fff701ba30b04bd76522cc04de0d8df24d6
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-31 14:42:51 +0200

    fix: printing (null) when passing a null string

--- print_funcs.c
+++ print_funcs.c
@@ -14,9 +14,13 @@ int print_char(va_list arg)
 {
 	int c = va_arg(arg, int);
 
-	write(1, &c, 1);
+	if (c)
+	{
+		write(1, &c, 1);
+		return (1);
+	}
 
-	return (1);
+	return (-1);
 }
 
 
@@ -32,6 +36,11 @@ int print_string(va_list arg)
 {
 	int i;
 	char *s = va_arg(arg, char *);
+
+	if (!s)
+	{
+		s = "(null)";
+	}
 
 	for (i = 0; s[i] != 0; i++)
 		write(1, &(s[i]), 1);
